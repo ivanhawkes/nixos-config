1aed906d0a9ac9ff24eb1f433581c21046da21ce
Ivan Hawkes <ivan.hawkes@gmail.com>
2023-09-08 22:19:40 +1000

    Updated the install script to remove existing files first.

--- users/ivan/.local/bin/install-scripts.sh
+++ users/ivan/.local/bin/install-scripts.sh
@@ -4,23 +4,29 @@
 
 
 ## Move in my typical shell scripts.
+mkdir $HOME/.local/bin
 
 # Shell configuration.
+rm $HOME/{.bashrc,.profile,.imwheel}
 ln -s $HOME/nixos-config/users/ivan/.bashrc $HOME/.bashrc
 ln -s $HOME/nixos-config/users/ivan/.profile $HOME/.profile
 ln -s $HOME/nixos-config/users/ivan/.imwheel $HOME/.imwheel
 
 # Clang formatting rules for VS Code C++.
+rm $HOME/.clang-format
 ln -s $HOME/nixos-config/users/ivan/.clang-format $HOME/.clang-format
 
 # Fast logins to SSH servers.
+rm $HOME/.local/bin/{de10-nano,dns-server,mercury,venus}
 ln -s $HOME/nixos-config/users/ivan/.local/bin/de10-nano $HOME/.local/bin/de10-nano
 ln -s $HOME/nixos-config/users/ivan/.local/bin/dns-server $HOME/.local/bin/dns-server
 ln -s $HOME/nixos-config/users/ivan/.local/bin/mercury $HOME/.local/bin/mercury
 ln -s $HOME/nixos-config/users/ivan/.local/bin/venus $HOME/.local/bin/venus
 
 # Janky backup.
+rm $HOME/.local/bin/backup.sh
 ln -s $HOME/nixos-config/users/ivan/.local/bin/backup.sh $HOME/.local/bin/backup.sh
 
 # Set the mouse speed and scrolling speed.
+rm $HOME/.local/bin/mousespeed.sh
 ln -s $HOME/nixos-config/users/ivan/.local/bin/mousespeed.sh $HOME/.local/bin/mousespeed.sh
