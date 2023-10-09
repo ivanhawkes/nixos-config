ce3c50fbe443a47c3ed148328369df44ea96e0c8
Ivan Hawkes <ivan.hawkes@gmail.com>
2023-10-10 00:20:48 +1000

    Added pico cloning command to save time.

---
+++ users/ivan/.local/bin/clone-pico-libraries.sh
@@ -0,0 +1,7 @@
+cd ~/projects
+git clone --recursive git@github.com:raspberrypi/openocd.git
+git clone --recursive git@github.com:raspberrypi/pico-sdk.git
+git clone --recursive git@github.com:raspberrypi/picotool.git
+git clone --recursive git@github.com:raspberrypi/pico-playground.git
+git clone --recursive git@github.com:raspberrypi/pico-extras.git
+git clone --recursive git@github.com:raspberrypi/picoprobe.git
