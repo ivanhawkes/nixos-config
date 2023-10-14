61fb9199bdff265b0ef08fd84ce851dc43e641ac
Ivan Hawkes <ivan.hawkes@gmail.com>
2023-10-14 16:56:49 +1000

    New backup script.

--- users/ivan/.local/bin/backup.sh
+++ users/ivan/.local/bin/backup.sh
@@ -1,31 +1,44 @@
 #!/bin/sh
 
+# Make sure the media drives are online. They don't mount automatically.
+#sudo mount /media/mercury
+#sudo mount /media/venus
+sudo mount -a
+
+echo Back up photos
+rsync -va --no-owner --no-group --size-only /media/media/photos/ /media/backup/photos/ --delete
+
 echo Back up anime
-rsync -va --no-owner --no-group --size-only /media/media/anime/ /mnt/venus/media/anime/ --delete
+rsync -va --no-owner --no-group --size-only /media/media/anime/ /media/backup/anime/ --delete
+
+echo Back up books
+rsync -va --no-owner --no-group --size-only /media/media/books/ /media/backup/books/ --delete
 
 echo Back up family media
-rsync -va --no-owner --no-group --size-only /media/media/family/ /mnt/venus/media/family/ --delete
+rsync -va --no-owner --no-group --size-only /media/media/family/ /media/backup/family/ --delete
 
 echo Back up films
-rsync -va --no-owner --no-group --size-only /media/data/film/ /mnt/venus/media/film/ --delete
+rsync -va --no-owner --no-group --size-only /media/media/film/ /media/backup/film/ --delete
 
 echo Back up music
-rsync -va --no-owner --no-group --size-only /media/media/music/ /mnt/venus/media/music/ --delete
-
-echo Back up photos.
-rsync -va --no-owner --no-group --size-only /media/media/photos/ /mnt/venus/media/photos/ --delete
-
-echo Back up training videos
-rsync -va --no-owner --no-group --size-only /media/media/training/ /mnt/venus/media/training/ --delete
+rsync -va --no-owner --no-group --size-only /media/media/music/ /media/backup/music/ --delete
 
 echo Back up tv shows
-rsync -va --no-owner --no-group --size-only /media/media/tv-series/ /mnt/mercury/media/tv-series/ --delete
+rsync -va --no-owner --no-group --size-only /media/media/tv-series/ /media/backup/tv-series/ --delete
 
 echo Back up videoclips
-rsync -va --no-owner --no-group --size-only /media/media/videoclip/ /mnt/venus/media/videoclip/ --delete
+rsync -va --no-owner --no-group --size-only /media/media/videoclip/ /media/backup/videoclip/ --delete
+
+echo Back up  audio library.
+rsync -va --no-owner --no-group --size-only /media/media/Audio Library/ /media/backup/Audio Library/ --delete
+
+echo Back up software.
+rsync -va --no-owner --no-group --size-only /media/media/software/ /media/backup/software/ --delete
+
+echo Back up drivers.
+rsync -va --no-owner --no-group --size-only /media/media/drivers/ /media/backup/drivers/ --delete
 
 
 # FIX PERMISSIONS
-sudo chown -R ivan:ivan /media/data/ && sudo chmod -R 0775 /media/data/ && sudo chmod -R 0700 /media/data/lost+found/
-sudo chown -R ivan:ivan /media/media/ && sudo chmod -R 0775 /media/media/ && sudo chmod -R 0700 /media/media/lost+found/
-
+#sudo chown -R nobody:nogroup /media/data/ && sudo chmod -R 0775 /media/data/ && sudo chmod -R 0700 /media/data/lost+found/
+#sudo chown -R nobody:nogroup /media/media/ && sudo chmod -R 0775 /media/media/ && sudo chmod -R 0700 /media/media/lost+found/
